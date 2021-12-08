541ca4bcd6c9a51112da16e7c3add36fe82d0b8b
Chonlawit penwijit <DISTSY@gmail.com>
2021-12-09 03:22:47 +0700

    api

--- controller/get.controller.js
+++ controller/get.controller.js
@@ -3,6 +3,5 @@ const userModel = require('../model/modelUser');
 const getData = async (req, res) => {
     const users = await userModel.find();
     res.json(users);
-
 };
 module.exports.getData = getData;
