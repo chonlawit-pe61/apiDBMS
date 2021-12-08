aa42947eed9e265e8738becf77d1babaade5fa4a
Chonlawit penwijit <DISTSY@gmail.com>
2021-12-09 03:44:07 +0700

    api_7

--- index.js
+++ index.js
@@ -3,5 +3,5 @@ const { API_PORT } = process.env;
 const port = process.env.PORT || API_PORT;
 
 app.listen(port, () => {
-  console.log("Sever is running on port "+ port);
+  console.log(`Server is listening on port ${port}`);
 });
