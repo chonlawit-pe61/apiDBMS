3d4f3c6edfe1bb63e37135b248405739c36ac0dc
Chonlawit penwijit <DISTSY@gmail.com>
2021-12-09 03:32:40 +0700

    api_3

--- app.js
+++ app.js
@@ -8,6 +8,13 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cors());
+app.use((req, res, next) => {
+    res.header({
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept',
+    });
+    next();
+})
 
 const router = require('./router/router');
 app.use('/api', router);
